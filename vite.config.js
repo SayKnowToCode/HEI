b471776bc19ba6065788ab80466dc99244968321
SoulDev07 <mayankravariya@gmail.com>
2024-10-20 00:34:21 +0530

    add vercel files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        unused: true,
+      },
+      mangle: true,
+      output: {
+        comments: false,
+      },
+    },
+  },
+});
