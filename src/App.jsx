05ede8b86301950c42e92faed7649197a21bd1b4
Overlord-1 <ruchirkhare12@gmail.com>
2025-01-18 16:17:51 +0530

    dash

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import LandingPage from '@/pages/LandingPage';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
       <Toaster position="bottom-right" richColors />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
       </Routes>
     </div>
   );
